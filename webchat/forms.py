a751cebea7bf73ba1a84f15c619715649e6ae73e
Mohammed <programmingwithmohammed21@gmail.com>
2022-02-12 23:22:39 +0000

    updated forms file

---
+++ webchat/forms.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from django import forms
+from .models import ChatTopic
+
+class NewChatTopicForm(forms.ModelForm):
+    message = forms.CharField(
+        widget = forms.Textarea(
+            attrs = {'rows': 7, 'placeholder':'What you are thinking'}
+        ),
+        max_length=5000,
+        help_text = 'The max length of the text is 5000.'
+    )
+
+    class Meta:
+        model = ChatTopic
+        fields = ['subject', 'message']
+
+
+
+
