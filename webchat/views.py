b779404e72b11d11ed988ad859a3a46ebc5d9534
Mohammed <programmingwithmohammed21@gmail.com>
2022-02-12 00:26:35 +0000

    refactor the views file and new board topic file aded new folder includes and form html file

--- webchat/views.py
+++ webchat/views.py
@@ -7,66 +7,38 @@ from .models import ChatBoard
 from .models import ChatTopic
 from .models import Post
 from django.contrib.auth.models import User
+from .forms import NewChatTopicForm
 
 
-
-# =============================================================================
-# def homepage(request):
-#     return HttpResponse('As salamu alykum..')
-#
-# =============================================================================
-
-# =============================================================================
-# def home(request):
-#     chatBoard = ChatBoard.objects.all()
-#     chatBoard_names = list()
-#
-#     for board in chatBoard:
-#         chatBoard_names.append(board.name)
-#
-#     response_html = '<br>'.join(chatBoard_names)
-#
-#     return HttpResponse(response_html)
-# =============================================================================
 
 def home(request):
     chatBoard = ChatBoard.objects.all()
     return render(request, 'home.html',{'chatBoard':chatBoard})
 
-
-#def about_comapny():
-#def about():
-
-# def board_topic(request):
-#     chat_board = ChatBoard.objects.all()
-#     return render(request, 'chat_topics.html', {'chat_board': chat_board})
-
 def board_topic(request,pk):
     chat_board = ChatBoard.objects.get(pk=pk)
     return render(request, 'chat_board_topics.html', {'chat_board': chat_board})
 
+
 def new_board_topic(request, pk):
     chat_board = get_object_or_404(ChatBoard, pk=pk)
 
+    user = User.objects.first()
+
     if request.method == 'POST':
-        subject = request.POST['subject']
-        message = request.POST['message']
+        form = NewChatTopicForm(request.POST)
+        if form.is_valid():
+            chatTopic = form.save(commit=False)
+            chatTopic.chat_board = chat_board
+            chatTopic.boardStarter = user
+            chatTopic.save()
 
-        user = User.objects.first()
+            post = Post.objects.create(
+                message=form.cleaned_data.get('message'),
+                topic=chatTopic,
+                createdBy=user)
+            return redirect('board_topic', pk=chat_board.pk)
+    else:
+        form = NewChatTopicForm()
 
-        chatTopic = ChatTopic.objects.create(
-            subject=subject,
-            boardName=chat_board,
-            boardStarter=user)
-
-        post = Post.objects.create(
-            message=message,
-            topic=chatTopic,
-            createdBy=user)
-        return redirect('board_topic', pk=chat_board.pk)
-
-
-    return render(request, 'new_board_topic.html', {'chat_board':chat_board})
-
-
-
+    return render(request, 'new_board_topic.html', {'chat_board':chat_board, 'form':form})
