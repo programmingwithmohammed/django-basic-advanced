20354477c2bab61f6efbb1fa3afab08caccf143c
Mohammed <programmingwithmohammed21@gmail.com>
2022-02-13 01:06:55 +0000

    Updated accounts/views file and base.html file and accounts/forms file

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login as auth_login_user
+from .forms import SignUpForm
 
 def signup(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login_user(request, user)
+            return redirect('home')
+    else:
+        #form = UserCreationForm()
+        form = SignUpForm()
+
     return render(request, 'signup.html', {'form':form})
